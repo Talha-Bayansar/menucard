1ba663cb8830c81df75ad2466690701dd59031f2
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-22 16:38:02 +0200

    Updates

---
+++ precache-manifest.de1c3b0b8d70278b2e11dcd39b6cebc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67efac61c7fe858db62979d2cd8276a",
+    "url": "/menucard/index.html"
+  },
+  {
+    "revision": "7f25de01377e99b458cb",
+    "url": "/menucard/static/css/main.f16214ab.chunk.css"
+  },
+  {
+    "revision": "258072d1d49380af24aa",
+    "url": "/menucard/static/js/2.e8e4106a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menucard/static/js/2.e8e4106a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f25de01377e99b458cb",
+    "url": "/menucard/static/js/main.8c7535bc.chunk.js"
+  },
+  {
+    "revision": "39f5a1cd608593fdedcb",
+    "url": "/menucard/static/js/runtime-main.52e723cb.js"
+  }
+]);
