d9fe540eeda17bbe47dd5d1b7c900085564fd9dc
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-22 15:48:24 +0200

    restructured files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import ProductLine from "./ProductLine";
-import Category from "./Category";
+import ProductLine from "./Components/ProductLine";
+import Category from "./Components/Category";
+import { PRODUCTS_DATA } from "./data/products_data";
 
 function App() {
-  const PRODUCTS_DATA = [
-    {
-      name: "drank",
-      products: [
-        { name: "cola", price: 2 },
-        { name: "water", price: 2 },
-        { name: "bier", price: 2, size: 25 },
-        { name: "wijn", price: 4 },
-      ],
-    },
-    {
-      name: "eten",
-      note: "een klein hapje voor de snelle honger",
-      products: [
-        { name: "chips", price: 1, note: "paprika of zout" },
-        { name: "olijven", price: 2, note: "mix zwart en groen" },
-        { name: "kaas", price: 4, note: "mix jonge en oude blokjes" },
-        { name: "salami", price: 3 },
-        { name: "hamburger", price: 4, note: "niet na 22:00" },
-        { name: "pizza", price: 4, note: "niet na 22:00" },
-      ],
-    },
-  ];
-
   return (
     <div className="App">
       <h1>Menu</h1>
@@ -39,14 +15,6 @@ function App() {
           ))}
         </Category>
       ))}
-      {/* {PRODUCTS_DATA.map((c) => (
-        <div>
-          <h2>{c.name}</h2>
-          {c.products.map((p) => (
-            <ProductLine key={p.name} product={p} />
-          ))}
-        </div>
-      ))} */}
     </div>
   );
 }
---
+++ src/data/products_data.js
@@ -0,0 +1,23 @@
+export const PRODUCTS_DATA = [
+  {
+    name: "drank",
+    products: [
+      { name: "cola", price: 2 },
+      { name: "water", price: 2 },
+      { name: "bier", price: 2, size: 25 },
+      { name: "wijn", price: 4 },
+    ],
+  },
+  {
+    name: "eten",
+    note: "een klein hapje voor de snelle honger",
+    products: [
+      { name: "chips", price: 1, note: "paprika of zout" },
+      { name: "olijven", price: 2, note: "mix zwart en groen" },
+      { name: "kaas", price: 4, note: "mix jonge en oude blokjes" },
+      { name: "salami", price: 3 },
+      { name: "hamburger", price: 4, note: "niet na 22:00" },
+      { name: "pizza", price: 4, note: "niet na 22:00" },
+    ],
+  },
+];
