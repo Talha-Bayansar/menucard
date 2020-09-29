74b8cfedec9d51aeac60316a3ce78d8c1140f22c
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-09-29 10:23:19 +0200

    oefening2.7 done

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -7,13 +7,21 @@ const StyledH1 = styled.h1`
   padding-top: 16px;
 `;
 
+const StyledCategoryNote = styled(Note)`
+  padding: 0 0 0.5rem 0;
+  background-color: red;
+`;
+
 function Category(props) {
   const { category } = props;
   return (
     <div>
       <StyledH1>{category.name}</StyledH1>
       {category.note && (
-        <Note className="note categroyNote" note={category.note} />
+        <StyledCategoryNote
+          className="note categroyNote"
+          note={category.note}
+        />
       )}
       {props.children}
     </div>
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const StyledNote = styled.div`
+  color: #a21212;
+  font-size: smaller;
+`;
 
 function Note(props) {
-  return <div className={props.className}>{props.note}</div>;
+  const { note, className } = props;
+  if (!note) return null;
+  return <StyledNote className={className}>{note}</StyledNote>;
 }
 
 export default Note;
