816c0e63abc99377a5078104758a7f219844649d
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-09 16:43:51 +0200

    implement extra info

--- src/components/product/ProductInfoBox.js
+++ src/components/product/ProductInfoBox.js
@@ -9,6 +9,7 @@ function ProductInfoBox(props) {
         <InfoBox isInfoBoxOpen={isInfoBoxOpen} closeInfoBox={closeInfoBox} >
           <h2>Info Box</h2>
           {activeProduct ? <ProductLine product={activeProduct} /> : "null"}
+          {activeProduct ? <p>{activeProduct.info}</p> : null}
         </InfoBox>
         </>
     )
